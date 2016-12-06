00251d9cc8bc708d3a0466b32514a946e2def4dc
Mario Fusco <mario.fusco@gmail.com>
2016-12-06 13:06:34 +0100

    [DROOLS-1353] move executable fluent builder to public api

--- kie-api/src/main/java/org/kie/api/runtime/builder/TaskFluent.java
+++ kie-api/src/main/java/org/kie/api/runtime/builder/TaskFluent.java
@@ -16,71 +16,11 @@
 package org.kie.api.runtime.builder;
 
 import org.kie.api.task.TaskService;
-import org.kie.api.task.model.OrganizationalEntity;
-import org.kie.api.task.model.Status;
-import org.kie.api.task.model.Task;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * See {@link TaskService}
  *
  */
 public interface TaskFluent<T> {
-
-    T activate(long taskId, String userId);
-
-    T claim(long taskId, String userId);
-
-    T claimNextAvailable(String userId, String language);
-
-    T complete(long taskId, String userId, Map<String, Object> data);
-
-    T delegate(long taskId, String userId, String targetUserId);
-
-    T exit(long taskId, String userId);
-
-    T fail(long taskId, String userId, Map<String, Object> faultData);
-
-    T forward(long taskId, String userId, String targetEntityId);
-
-    T getTaskByWorkItemId(long workItemId);
-
-    T getTaskById(long taskId);
-
-    T getTasksAssignedAsBusinessAdministrator(String userId, String language);
-
-    T getTasksAssignedAsPotentialOwner(String userId, String language);
-
-    T getTasksAssignedAsPotentialOwnerByStatus(String userId, List<Status> status, String language);
-
-    T getTasksOwned(String userId, String language);
-
-    T getTasksOwnedByStatus(String userId, List<Status> status, String language);
-
-    T getTasksByStatusByProcessInstanceId(long processInstanceId, List<Status> status, String language);
-
-    T getTasksByProcessInstanceId(long processInstanceId);
-
-    T addTask(Task task, Map<String, Object> params);
-
-    T release(long taskId, String userId);
-
-    T resume(long taskId, String userId);
-
-    T skip(long taskId, String userId);
-
-    T start(long taskId, String userId);
-
-    T stop(long taskId, String userId);
-
-    T suspend(long taskId, String userId);
-
-    T nominate(long taskId, String userId, List<OrganizationalEntity> potentialOwners);
-
-    T getContentById(long contentId);
-
-    T getAttachmentById(long attachId);
 
 }
