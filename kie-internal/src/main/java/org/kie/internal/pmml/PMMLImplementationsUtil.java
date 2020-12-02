1e122ddfb945cc047df69c874d905dad3f525f4b
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2020-12-02 15:47:38 +0100

    [DROOLS-5867] Fixed as per PR requests

--- kie-internal/src/main/java/org/kie/internal/pmml/PMMLImplementationsUtil.java
+++ kie-internal/src/main/java/org/kie/internal/pmml/PMMLImplementationsUtil.java
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kie.api.pmml;
+package org.kie.internal.pmml;
 
+import org.kie.api.pmml.PMMLConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
